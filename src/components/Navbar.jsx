a895308e231bdaabadf9d0ae2c79c2f5f2aced14
TLAMHutto <keatonhutto8@gmail.com>
2022-08-26 16:51:09 -0500

    nav styln

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../assets/css/navbar.css';
+import '../App.css';
 
 function NavBar() {
   return (
     <>
-      <Navbar className='navbar-main' variant="dark">
+      <Navbar className='navbar' variant="dark">
         <Container className='navbar-main'>
           <Navbar.Brand className='title' href="/">My Arduino Builds</Navbar.Brand>
           <Nav className="me-auto">
