3343aa3d9033960743cfbc2126087c5ab765e0bc
TLAMHutto <keatonhutto8@gmail.com>
2022-08-29 13:51:41 -0500

    home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css'
 import './index.css'
 import App from './App'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 // import { AppProvider } from './context/appContext'
 
 ReactDOM.render(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 import '../assets/css/home.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import '../App.css';
+
 
 function Home() {
   return (
-    <div>
-      Home Page
+    <>
+    <div className='container-home'>
+      <h1 className="about-title">Welcome to my Arduino build page</h1>
+      <p className="about-text">I made this page to track and document and share various projects that I have created and finished using arduino's single board computer. Hopfully these projects will help anyone searching for similar ideas or interests</p>
+      <p className='github-link'><a href='https://github.com/TLAMHutto' target='_blank'>My GitHub</a></p>
     </div>
+    </>
   )
 }
 
